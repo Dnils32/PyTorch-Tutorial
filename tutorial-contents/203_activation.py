93a37d93042aae9c9aeff77e13c1d1f0bc40fee7
DumbZarro <56104025+DumbZarro@users.noreply.github.com>
2020-08-05 14:52:50 +0800

    增加了注释

--- tutorial-contents/203_activation.py
+++ tutorial-contents/203_activation.py
@@ -1,27 +1,26 @@
-"""
-View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-
-Dependencies:
-torch: 0.4
-matplotlib
-"""
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
 # fake data
-x = torch.linspace(-5, 5, 200)  # x data (tensor), shape=(100, 1)
-x = Variable(x)
-x_np = x.data.numpy()   # numpy array for plotting
+x = torch.linspace(-5, 5, 200)  # Returns a one-dimensional tensor
+'''
+arange()和linspace()通过指定开始值、终值创建表示等差数列的一维数组
+arange()还需要步长作为参数,得到的结果数组不包含终值。
+linspace()还需要元素个数作为参数，可以通过endpoint参数指定是否包含终值，默认为True包含终值。
+'''
+print(x.size())    # torch.Size([200])
+x_np = x.numpy()  # numpy array for plotting
+# x_np = x.data.numpy()  # 加data的是过去的写法了,也不会报错.
 
 # following are popular activation functions
-y_relu = torch.relu(x).data.numpy()
-y_sigmoid = torch.sigmoid(x).data.numpy()
-y_tanh = torch.tanh(x).data.numpy()
-y_softplus = F.softplus(x).data.numpy() # there's no softplus in torch
-# y_softmax = torch.softmax(x, dim=0).data.numpy() softmax is a special kind of activation function, it is about probability
+y_relu = torch.relu(x).numpy()
+y_sigmoid = torch.sigmoid(x).numpy()
+y_tanh = torch.tanh(x).numpy()
+y_softplus = F.softplus(x).numpy()  # there's no softplus in torch
+# y_softmax = torch.softmax(x, dim=0).data.numpy()
+# softmax is a special kind of activation function, it is about probability
 
 # plt to visualize these activation function
 plt.figure(1, figsize=(8, 6))
