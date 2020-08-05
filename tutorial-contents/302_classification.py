b0ff9f9780d0996a8ee692cb1a8a8ed3112190c0
DumbZarro <56104025+DumbZarro@users.noreply.github.com>
2020-08-05 15:04:45 +0800

    增加了注释

--- tutorial-contents/302_classification.py
+++ tutorial-contents/302_classification.py
@@ -1,11 +1,3 @@
-"""
-View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
-My Youtube Channel: https://www.youtube.com/user/MorvanZhou
-
-Dependencies:
-torch: 0.4
-matplotlib
-"""
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
@@ -14,12 +6,16 @@ import matplotlib.pyplot as plt
 
 # make fake data
 n_data = torch.ones(100, 2)
-x0 = torch.normal(2*n_data, 1)      # class0 x data (tensor), shape=(100, 2)
-y0 = torch.zeros(100)               # class0 y data (tensor), shape=(100, 1)
-x1 = torch.normal(-2*n_data, 1)     # class1 x data (tensor), shape=(100, 2)
-y1 = torch.ones(100)                # class1 y data (tensor), shape=(100, 1)
+# 生成第一部分数据,使用正态分布,并且数值都为0
+x0 = torch.normal(2 * n_data, 1)  # class0 x data (tensor), shape=(100, 2)
+y0 = torch.zeros(100)  # class0 y data (tensor), shape=(100, 1)
+# 生成第二部分数据,使用正态分布,并且数值都为1
+x1 = torch.normal(-2 * n_data, 1)  # class1 x data (tensor), shape=(100, 2)
+y1 = torch.ones(100)  # class1 y data (tensor), shape=(100, 1)
+# 将两部分的数据合成
 x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating
-y = torch.cat((y0, y1), ).type(torch.LongTensor)    # shape (200,) LongTensor = 64-bit integer
+y = torch.cat((y0, y1), ).type(torch.LongTensor)  # shape (200,) LongTensor = 64-bit integer
+
 
 # The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
 # x, y = Variable(x), Variable(y)
@@ -28,32 +24,33 @@ y = torch.cat((y0, y1), ).type(torch.LongTensor)    # shape (200,) LongTensor = 
 # plt.show()
 
 
-class Net(torch.nn.Module):
+class Net(torch.nn.Module):     # 搭建神经网络
     def __init__(self, n_feature, n_hidden, n_output):
-        super(Net, self).__init__()
-        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
-        self.out = torch.nn.Linear(n_hidden, n_output)   # output layer
+        super(Net, self).__init__()     # 继承父类属性
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)  # hidden layer
+        self.out = torch.nn.Linear(n_hidden, n_output)  # output layer
 
-    def forward(self, x):
-        x = F.relu(self.hidden(x))      # activation function for hidden layer
-        x = self.out(x)
-        return x
+    def forward(self, data):
+        data = F.relu(self.hidden(data))  # activation function for hidden layer
+        data = self.out(data)   # 输出
+        return data
 
-net = Net(n_feature=2, n_hidden=10, n_output=2)     # define the network
+
+net = Net(n_feature=2, n_hidden=10, n_output=2)  # define the network
 print(net)  # net architecture
 
 optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
 loss_func = torch.nn.CrossEntropyLoss()  # the target label is NOT an one-hotted
-
-plt.ion()   # something about plotting
+# One-Hot编码，又称为一位有效编码，主要是采用N位状态寄存器来对N个状态进行编码，每个状态都由他独立的寄存器位，并且在任意时候只有一位有效。
+# 和301几乎一样,只有损失函数是不一样的,分类问题结果应该是one-hot的,不适合用MSE应该用交叉熵
+plt.ion()  # something about plotting
 
 for t in range(100):
-    out = net(x)                 # input x and predict based on x
-    loss = loss_func(out, y)     # must be (1. nn output, 2. target), the target label is NOT one-hotted
-
-    optimizer.zero_grad()   # clear gradients for next train
-    loss.backward()         # backpropagation, compute gradients
-    optimizer.step()        # apply gradients
+    out = net(x)  # input x and predict based on x
+    loss = loss_func(out, y)  # must be (1. nn output, 2. target), the target label is NOT one-hotted
+    optimizer.zero_grad()  # clear gradients for next train
+    loss.backward()  # backpropagation, compute gradients
+    optimizer.step()  # apply gradients
 
     if t % 2 == 0:
         # plot and show learning process
@@ -63,7 +60,7 @@ for t in range(100):
         target_y = y.data.numpy()
         plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
         accuracy = float((pred_y == target_y).astype(int).sum()) / float(target_y.size)
-        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
+        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color': 'red'})
         plt.pause(0.1)
 
 plt.ioff()
